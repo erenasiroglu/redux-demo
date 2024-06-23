aa1954b19c056967500473bedd33fcd4a976ddd4
Eren Nasıroğlu <erenasiroglu1@gmail.com>
2024-06-23 17:21:51 +0300

    created the project and header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ marginBottom: 10 }}>
+      <h1>My App</h1>
+      <button>Chande the dark mode</button>
+    </header>
+  );
+};
+
+export default Header;
